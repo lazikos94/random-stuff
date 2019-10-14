92875bb1b0728d46be52cd64785dadb7161866d0
lazikos94 <lazikos94@gmail.com>
2019-10-14 17:59:41 +0300

    changes to the react folder

--- public/react/react.js
+++ public/react/react.js
@@ -1,25 +1,61 @@
-'use strict';
-
-const e = React.createElement;
-
-class LikeButton extends React.Component {
-  constructor(props) {
+class Form extends React.Component{
+  constructor(props){
     super(props);
-    this.state = { liked: false };
+    this.state = {
+      firstname: '',
+      lastname: '',
+      age: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    
   }
 
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
+  handleChange(event) {
+    const target = event.target;
+    const firstname = target.name;
+    const lastname = target.name;
+    const age = target.name;
+    const value = target.value;
 
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
+    this.setState({
+      [firstname]:value,
+      [lastname]:value,
+      [age]:value,
+    });
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.firstname +' ' + this.state.lastname +' ' + this.state.age);
+    event.preventDefault();
+  }
+
+  render(){
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          FirstName:
+          <input name="firstname" type="text" value={this.state.firstname} onChange={this.handleChange} />
+        </label>
+        <br/>
+        <label>
+          LastName:
+          <input name="lastname" type="text" value={this.state.lastname} onChange={this.handleChange} />
+        </label>
+        <br/>
+        <label>
+          Age:
+          <input name="age" type="number" value={this.state.age} onChange={this.handleChange} />
+        </label>
+        <br/>
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
 
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(
+  <Form />,
+  document.getElementById('root')
+);
